370c3d6de32fa16754cf6a9971952dbe929165df
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-05-27 23:05:52 +0100

    comiit homework react redux toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import React from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './FilterSearch/FilterSearch';
+import Section from './Section/Section';
+import { getContacts } from '../redux/selectors';
+import { useSelector } from 'react-redux';
+
 export const App = () => {
+  const contacts = useSelector(getContacts);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook">
+        <ContactForm />
+        <Filter />
+        <ContactList />
+      </Section>
+    </>
   );
 };
+
+export default App;
